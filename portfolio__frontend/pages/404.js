a0a8e675a31a233df431b05a39b98717f48fdced
Ivan Atias <ivan.d.atias@gmail.com>
2022-06-12 20:16:14 -0400

    add custom 404 page

---
+++ portfolio__frontend/pages/404.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
+
+const NotFoundPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Ivan Atias - 404 Not Found</title>
+      </Head>
+      <div className="text-black dark:text-white h-screen grid place-content-center gap-3">
+        <h1 className="font-bold text-4xl 2xl:text-5xl">Page not found</h1>
+        <Link href="/">
+          <a className="text-base 2xl:text-lg underline font-semibold text-center">
+            Go back home
+          </a>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default NotFoundPage;
