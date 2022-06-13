47052789c92fb4cdd103c3fd178baf6c3bd0d73b
Ivan Atias <ivan.d.atias@gmail.com>
2022-06-12 20:15:58 -0400

    remove underline from Article titles

--- portfolio__frontend/components/Article.jsx
+++ portfolio__frontend/components/Article.jsx
@@ -12,7 +12,6 @@ const Article = ({ children, title, delay = 0 }) => {
     >
       <h2 className="relative text-black w-fit dark:text-gray-100 font-bold text-xl md:text-2xl">
         {title}
-        <span className="absolute left-0 bottom-[-4px] h-[2px] w-full bg-indigo-700 dark:bg-indigo-600"></span>
       </h2>
       {children}
     </motion.article>
