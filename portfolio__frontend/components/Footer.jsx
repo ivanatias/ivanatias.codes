20caf1a85a489a185f6bf771f26e5ac26462be8d
Ivan Atias <ivan.d.atias@gmail.com>
2022-06-19 00:48:31 -0400

    correcting footer heading text

--- portfolio__frontend/components/Footer.jsx
+++ portfolio__frontend/components/Footer.jsx
@@ -2,23 +2,23 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="dark:bg-neutral-900 flex flex-col justify-between items-center pt-12 pb-6">
+    <footer className="flex flex-col items-center justify-between pt-12 pb-6 dark:bg-neutral-900">
       <div className="flex flex-col gap-5 max-w-[824px] w-full mx-auto px-4 md:px-5">
-        <h2 className=" text-black  dark:text-gray-100 font-bold text-xl md:text-2xl">
-          Feel free to reach me out!
+        <h2 className="text-xl font-bold text-black  dark:text-gray-100 md:text-2xl">
+          Feel free to reach out to me!
         </h2>
         <p className="text-black dark:text-gray-300 2xl:text-lg">
           Contact me through my email to talk about development, design or any
           other subject.
         </p>
         <a
-          className="text-black font-bold dark:text-gray-300 text-base 2xl:text-lg"
+          className="text-base font-bold text-black dark:text-gray-300 2xl:text-lg"
           href="mailto:ivan.d.atias@gmail.com"
         >
           ivan.d.atias@gmail.com
         </a>
       </div>
-      <small className="mt-5 font-semibold text-black dark:text-gray-500 text-xs 2xl:text-sm">
+      <small className="mt-5 text-xs font-semibold text-black dark:text-gray-500 2xl:text-sm">
         © {new Date().getFullYear()} Ivan Atias. All rights reserved.
       </small>
     </footer>
