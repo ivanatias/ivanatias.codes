743eb35891f6b0dd5377b6543242be4a7afcb0e4
Ivan Atias <ivan.d.atias@gmail.com>
2022-06-27 22:53:35 -0400

    create custom code component

---
+++ portfolio__frontend/components/CustomCode.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Refractor from "react-refractor";
+//Languages
+import javascript from "refractor/lang/javascript";
+import typescript from "refractor/lang/typescript";
+import jsx from "refractor/lang/jsx";
+import tsx from "refractor/lang/tsx";
+import css from "refractor/lang/css";
+import sass from "refractor/lang/sass";
+import scss from "refractor/lang/scss";
+import batch from "refractor/lang/batch";
+import json from "refractor/lang/json";
+import xml from "refractor/lang/xml-doc";
+
+//Register languages
+Refractor.registerLanguage(javascript);
+Refractor.registerLanguage(typescript);
+Refractor.registerLanguage(jsx);
+Refractor.registerLanguage(tsx);
+Refractor.registerLanguage(css);
+Refractor.registerLanguage(sass);
+Refractor.registerLanguage(scss);
+Refractor.registerLanguage(batch);
+Refractor.registerLanguage(json);
+Refractor.registerLanguage(xml);
+
+const CustomCode = ({ value }) => {
+  return (
+    <div className="w-full xxs:max-w-[280px] xs:max-w-[335px] prevsm:max-w-[460px] sm:max-w-[600px] md:max-w-full mx-auto">
+      <Refractor language={value?.code?.language} value={value?.code?.code} />
+    </div>
+  );
+};
+
+export default CustomCode;
