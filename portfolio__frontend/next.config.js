01c0411c9f6ae4be080b09b6245f2dd8aafad9ff
Ivan Atias <ivan.d.atias@gmail.com>
2022-07-01 03:38:03 -0400

    add images sizes for better performance when serving images

--- portfolio__frontend/next.config.js
+++ portfolio__frontend/next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["cdn.sanity.io"],
-    deviceSizes: [520, 560, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    deviceSizes: [520, 560, 640, 768, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 56, 64, 96, 128, 150, 256, 384],
   },
 };
 
