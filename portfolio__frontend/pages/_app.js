b7da8a3997681db3f119b80013cc3b76c26b4d81
Ivan Atias <ivan.d.atias@gmail.com>
2022-07-02 16:09:18 -0400

    lazy load layout component

--- portfolio__frontend/pages/_app.js
+++ portfolio__frontend/pages/_app.js
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 import { ThemeProvider } from "next-themes";
-import { Layout } from "../components";
+const DynamicLayout = dynamic(() => import("../components/layout/Layout.jsx"));
 const DynamicAnimatePresence = dynamic(() =>
   import("../components/AnimateTransition")
 );
@@ -10,11 +10,11 @@ import "../styles/prism-theme.css";
 function MyApp({ Component, pageProps, router }) {
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
-      <Layout router={router}>
+      <DynamicLayout router={router}>
         <DynamicAnimatePresence>
           <Component {...pageProps} key={router.asPath} />
         </DynamicAnimatePresence>
-      </Layout>
+      </DynamicLayout>
     </ThemeProvider>
   );
 }
