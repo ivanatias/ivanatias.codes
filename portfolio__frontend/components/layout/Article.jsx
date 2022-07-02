35a6b3143548433f7246f93ac20c69160ccd1bdc
Ivan Atias <ivan.d.atias@gmail.com>
2022-07-01 23:21:49 -0400

    lazy loading motion elements

--- portfolio__frontend/components/layout/Article.jsx
+++ portfolio__frontend/components/layout/Article.jsx
@@ -1,19 +1,26 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { LazyMotion, m } from "framer-motion";
+
+const loadFeatures = () =>
+  import("../../constants/animateFeatures").then((res) => res.default);
 
 const Article = ({ children, title, delay = 0 }) => {
   return (
-    <motion.article
-      className="flex flex-col w-full gap-4"
-      initial={{ opacity: 0, y: 10 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.6, delay: delay }}
-    >
-      <h2 className="text-xl font-bold text-black dark:text-gray-100 md:text-2xl">
-        {title}
-      </h2>
-      {children}
-    </motion.article>
+    <LazyMotion features={loadFeatures} strict>
+      <m.article
+        className="flex flex-col w-full gap-4"
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: delay }}
+      >
+        {title && (
+          <h2 className="text-xl font-bold text-black dark:text-gray-100 md:text-2xl">
+            {title}
+          </h2>
+        )}
+        {children}
+      </m.article>
+    </LazyMotion>
   );
 };
 
--- portfolio__frontend/components/layout/Section.jsx
+++ portfolio__frontend/components/layout/Section.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { MetaData } from "../../components";
-import { motion } from "framer-motion";
+import { LazyMotion, m } from "framer-motion";
+
+const loadFeatures = () =>
+  import("../../constants/animateFeatures").then((res) => res.default);
 
 const variants = {
   hidden: { opacity: 0, x: 0, y: 20 },
@@ -25,16 +28,18 @@ const Section = ({
         socialCardImage={socialCardImage}
         contentType={contentType}
       />
-      <motion.section
-        className="flex flex-col gap-10"
-        initial="hidden"
-        animate="enter"
-        exit="exit"
-        variants={variants}
-        transition={{ duration: 0.4, type: "easeInOut" }}
-      >
-        {children}
-      </motion.section>
+      <LazyMotion features={loadFeatures} strict>
+        <m.section
+          className="flex flex-col gap-10"
+          initial="hidden"
+          animate="enter"
+          exit="exit"
+          variants={variants}
+          transition={{ duration: 0.4, type: "easeInOut" }}
+        >
+          {children}
+        </m.section>
+      </LazyMotion>
     </>
   );
 };
