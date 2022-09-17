d490f47a0bcdfc3e7f292eb0b357b333d7816e83
Ivan Atias <ivan.d.atias@gmail.com>
2022-09-17 18:41:46 -0400

    refactor: modify device sizes in next-config

--- portfolio__frontend/next.config.js
+++ portfolio__frontend/next.config.js
@@ -7,7 +7,9 @@ module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   images: {
     domains: ['cdn.sanity.io'],
-    deviceSizes: [520, 560, 640, 768, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 56, 64, 96, 128, 150, 256, 384]
+    deviceSizes: [
+      640, 750, 828, 1080, 1200, 1920, 2048, 2400, 2800, 3200, 3840
+    ],
+    imageSizes: [16, 32, 48, 56, 64, 96, 128, 150, 256, 384, 520]
   }
 })
