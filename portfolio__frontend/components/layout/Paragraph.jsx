6cb5a4536d40ab8125387f60947b2aa20c154f01
Ivan Atias <ivan.d.atias@gmail.com>
2022-11-29 23:18:04 -0400

    refactor(Paragraph): include semibold prop and utility class

--- portfolio__frontend/components/layout/Paragraph.jsx
+++ portfolio__frontend/components/layout/Paragraph.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const Paragraph = ({ children }) => {
-  return (
-    <p className={`text-black dark:text-gray-300 text-base 2xl:text-lg`}>
-      {children}
-    </p>
-  )
+const Paragraph = ({ children, semibold }) => {
+  const paragraphStyles = semibold ? 'semi-bold-paragraph' : 'normal-paragraph'
+
+  return <p className={paragraphStyles}>{children}</p>
 }
 
 export default Paragraph
