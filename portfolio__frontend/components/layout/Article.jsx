3dc50f4b52a3c8391c73910c8385692d45607ca6
Ivan Atias <ivan.d.atias@gmail.com>
2022-11-30 01:45:55 -0400

    refactor(Article): refactor component according to changes in Title component

--- portfolio__frontend/components/layout/Article.jsx
+++ portfolio__frontend/components/layout/Article.jsx
@@ -1,16 +1,8 @@
 import React from 'react'
-import Title from '@/components/layout/Title'
 import { motion } from 'framer-motion'
 
 export const Article = ({ children }) => (
   <article className='flex flex-col w-full gap-4'>{children}</article>
-)
-
-export const ArticleWithTitle = ({ children, title, largeTitle, titleTag }) => (
-  <Article>
-    <Title title={title} largeTitle={largeTitle} titleTag={titleTag} />
-    {children}
-  </Article>
 )
 
 export const AnimatedArticle = ({ children, delay = 0 }) => (
@@ -21,17 +13,4 @@ export const AnimatedArticle = ({ children, delay = 0 }) => (
   >
     <Article>{children}</Article>
   </motion.div>
-)
-
-export const AnimatedArticleWithTitle = ({
-  children,
-  title,
-  largeTitle,
-  titleTag,
-  delay = 0
-}) => (
-  <AnimatedArticle delay={delay}>
-    <Title title={title} largeTitle={largeTitle} titleTag={titleTag} />
-    {children}
-  </AnimatedArticle>
 )
