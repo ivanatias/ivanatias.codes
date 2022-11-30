c3b4a1c9af65576d1d773f3c209e4a7122b2873d
Ivan Atias <ivan.d.atias@gmail.com>
2022-11-30 01:44:27 -0400

    refactor(Title): separate titles into different components by sizes

--- portfolio__frontend/components/layout/Title.jsx
+++ portfolio__frontend/components/layout/Title.jsx
@@ -1,25 +1,57 @@
 import React from 'react'
 
-const Title = ({ title, largeTitle, xlTitle, titleTag }) => {
-  const titleStyles = largeTitle
-    ? 'large-title'
-    : xlTitle
-    ? 'xl-title'
-    : 'normal-title'
-
+export const Title = ({ children, titleTag, styles, semibold }) => {
   if (titleTag === 'h2') {
-    return <h2 className={titleStyles}>{title}</h2>
+    return (
+      <h2 className={`${styles} ${semibold ? 'font-semibold' : 'font-bold'}`}>
+        {children}
+      </h2>
+    )
   }
 
   if (titleTag === 'h3') {
-    return <h3 className={titleStyles}>{title}</h3>
+    return (
+      <h3 className={`${styles} ${semibold ? 'font-semibold' : 'font-bold'}`}>
+        {children}
+      </h3>
+    )
   }
 
   if (titleTag === 'h4') {
-    return <h4 className={titleStyles}>{title}</h4>
+    return (
+      <h4 className={`${styles} ${semibold ? 'font-semibold' : 'font-bold'}`}>
+        {children}
+      </h4>
+    )
   }
 
-  return <h1 className={titleStyles}>{title}</h1>
+  return (
+    <h1 className={`${styles} ${semibold ? 'font-semibold' : 'font-bold'}`}>
+      {children}
+    </h1>
+  )
 }
 
-export default Title
+export const SmallTitle = ({ children, titleTag, semibold }) => (
+  <Title titleTag={titleTag} semibold={semibold} styles='small-title'>
+    {children}
+  </Title>
+)
+
+export const NormalTitle = ({ children, titleTag, semibold }) => (
+  <Title titleTag={titleTag} semibold={semibold} styles='normal-title'>
+    {children}
+  </Title>
+)
+
+export const LargeTitle = ({ children, titleTag, semibold }) => (
+  <Title titleTag={titleTag} semibold={semibold} styles='large-title'>
+    {children}
+  </Title>
+)
+
+export const XLargeTitle = ({ children, titleTag, semibold }) => (
+  <Title titleTag={titleTag} semibold={semibold} styles='xl-title'>
+    {children}
+  </Title>
+)
