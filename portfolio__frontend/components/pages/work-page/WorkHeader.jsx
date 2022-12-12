c44803374b3fc0bf2175b7c10065da916a6aef1a
Ivan Atias <ivan.d.atias@gmail.com>
2022-12-12 00:36:22 -0400

    refactor(WorkHeader): remove react-icons

--- portfolio__frontend/components/pages/work-page/WorkHeader.jsx
+++ portfolio__frontend/components/pages/work-page/WorkHeader.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import CustomLink from '@/components/CustomLink'
-import { HiChevronRight } from 'react-icons/hi'
 
 const WorkHeader = ({ workTitle }) => (
   <div className='flex items-center gap-2'>
     <CustomLink href='/works'>
       <a>
-        <h2 className='text-sm font-bold text-black dark:text-white 2xl:text-base'>
+        <h2 className='text-base font-bold text-black dark:text-white 2xl:text-lg'>
           Works
         </h2>
       </a>
     </CustomLink>
-    <HiChevronRight
-      fontSize={20}
-      className='text-black font-bold dark:text-white ml-[-4px]'
-    />
+    <span
+      className='text-sm font-semibold text-black dark:text-white'
+      aria-hidden={true}
+    >
+      |
+    </span>
     <h3 className='text-base font-bold text-pink-800 dark:text-pink-600 2xl:text-lg'>
       {workTitle}
     </h3>
