0aa97d06cb717f9200b3deaf181decbfab1410fa
Ivan Atias <ivan.d.atias@gmail.com>
2022-12-12 02:07:05 -0400

    refactor(WorkLinks): replace react-icons with SVG components

--- portfolio__frontend/components/pages/work-page/WorkLinks.jsx
+++ portfolio__frontend/components/pages/work-page/WorkLinks.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AiOutlineGithub, AiOutlineEye } from 'react-icons/ai'
+import SVG from '@/components/SVG'
 
 const WorkLinks = ({ githubUrl, projectUrl }) => (
   <div className='flex flex-col gap-3 mt-2'>
@@ -7,17 +7,19 @@ const WorkLinks = ({ githubUrl, projectUrl }) => (
       href={githubUrl}
       target='_blank'
       rel='noreferrer noopener'
-      className='flex items-center gap-2 hover:underline w-fit'
+      className='flex items-center gap-2 text-black dark:text-gray-100 hover:underline w-fit'
     >
-      <AiOutlineGithub fontSize={24} /> Source code
+      <SVG id='icon-github' hidden className='w-5 h-5' />
+      Source code
     </a>
     <a
       href={projectUrl}
       target='_blank'
       rel='noreferrer noopener'
-      className='flex items-center gap-2 hover:underline w-fit'
+      className='flex items-center gap-2 text-black dark:text-gray-100 hover:underline w-fit'
     >
-      <AiOutlineEye fontSize={24} /> Live project
+      <SVG id='icon-eye' hidden className='w-5 h-5' />
+      Live project
     </a>
   </div>
 )
