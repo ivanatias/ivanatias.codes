cd4026e4e109c4f24e1c79ac9d69bca5089c02ab
Ivan Atias <ivan.d.atias@gmail.com>
2022-12-12 02:38:44 -0400

    refactor(ThemeToggle): modify toggle button styling and implement SVG component

--- portfolio__frontend/components/ThemeToggle.jsx
+++ portfolio__frontend/components/ThemeToggle.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
+import SVG from '@/components/SVG'
 import { useTheme } from 'next-themes'
 import { motion, AnimatePresence } from 'framer-motion'
 
+const themeOptions = {
+  dark: { label: 'Light mode', icon: 'icon-sun', iconLabel: 'Sun icon' },
+  light: { label: 'Dark mode', icon: 'icon-moon', iconLabel: 'Moon icon' }
+}
+
 const ThemeToggle = () => {
   const { theme, setTheme } = useTheme()
+  const { label, icon, iconLabel } = themeOptions[theme]
 
   return (
     <AnimatePresence mode='wait' initial={false}>
       <motion.button
-        className='flex items-center justify-center w-12 h-10 bg-indigo-700 border-none rounded-md outline-none cursor-pointer dark:bg-orange-600'
+        className='flex items-center justify-center w-12 h-10 hover:drop-shadow-[0_0_12px] text-yellow-400 border-none outline-none cursor-pointer dark:text-orange-400'
         onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-        aria-label='Toggle Theme'
+        aria-label={label}
         key={theme}
         initial={{ y: -20, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         exit={{ y: 20, opacity: 0 }}
         transition={{ duration: 0.2 }}
       >
-        <p aria-hidden={true} className='text-sm md:text-base'>
-          {theme === 'light' ? '🌙' : '☀️'}
-        </p>
+        <SVG id={icon} title={iconLabel} className='w-5 h-5' />
       </motion.button>
     </AnimatePresence>
   )
