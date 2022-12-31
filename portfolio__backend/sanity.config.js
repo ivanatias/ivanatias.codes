02317a9676e90f8e3fb2958a573fe866dfad4b87
Ivan Atias <ivan.d.atias@gmail.com>
2022-12-31 00:38:13 -0400

    feat: add sanity plugin media

--- portfolio__backend/sanity.config.js
+++ portfolio__backend/sanity.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { codeInput } from "@sanity/code-input";
+import { media } from "sanity-plugin-media";
 import schemas from "./schemas/schema";
 
 export default defineConfig({
   title: "Ivan Atias personal website content manager",
   projectId: "uaj0umn5",
   dataset: "production",
-  plugins: [deskTool(), visionTool(), codeInput()],
+  plugins: [deskTool(), visionTool(), codeInput(), media()],
   tools: (prev) => {
     return import.meta.env.DEV === true
       ? prev
