d1b1e58cd4b58902299f96913799cdc328e12c5f
Ivan Atias <ivan.d.atias@gmail.com>
2023-02-01 19:12:07 -0400

    refactor(MetaData): place constants outside of component

--- portfolio__frontend/components/MetaData.jsx
+++ portfolio__frontend/components/MetaData.jsx
@@ -1,64 +1,62 @@
 import Head from 'next/head'
 
+const defaultTitle = 'Ivan Atias · Front-end Engineer, UI Designer'
+const defaultDescription =
+  'Ivan Atias is a Front-end Engineer and UI Designer who enjoys a lot building good looking and functional websites and apps.'
+const defaultImage = 'https://www.ivanatias.codes/card.png'
+const defaultOgType = 'website'
+
 const MetaData = ({
   title,
   description,
   canonicalUrlPath,
   socialCardImage,
   contentType
-}) => {
-  const defaultTitle = 'Ivan Atias · Front-end Engineer, UI Designer'
-  const defaultDescription =
-    'Ivan Atias is a Front-end Engineer and UI Designer who enjoys a lot building good looking and functional websites and apps.'
-  const defaultImage = 'https://www.ivanatias.codes/card.png'
-  const defaultOgType = 'website'
-
-  return (
-    <Head>
-      <title>{title ? `${title} - Ivan Atias` : defaultTitle}</title>
-      <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <meta charSet='UTF-8' />
-      <meta name='description' content={description || defaultDescription} />
-      <meta
-        name='keywords'
-        content='Front-end Engineer, UI Designer, Ivan Atias, Portfolio, Blog'
-      />
-      <meta name='author' content='Ivan Atias' />
-      <link
-        rel='canonical'
-        href={`https://www.ivanatias.codes${canonicalUrlPath || ''}`}
-      />
-      <meta
-        property='og:url'
-        content={`https://www.ivanatias.codes${canonicalUrlPath || ''}`}
-      />
-      <meta
-        property='og:title'
-        content={title ? `${title} - Ivan Atias` : defaultTitle}
-      />
-      <meta
-        property='og:description'
-        content={description || defaultDescription}
-      />
-      <meta name='twitter:card' content='summary_large_image' />
-      <meta property='og:site_name' content='Ivan Atias Website' />
-      <meta
-        name='twitter:title'
-        content={title ? `${title} - Ivan Atias` : defaultTitle}
-      />
-      <meta
-        name='twitter:description'
-        content={description || defaultDescription}
-      />
-      <meta
-        name='image'
-        property='og:image'
-        content={socialCardImage || defaultImage}
-      />
-      <meta name='twitter:image' content={socialCardImage || defaultImage} />
-      <meta property='og:type' content={contentType || defaultOgType} />
-    </Head>
-  )
-}
+}) => (
+  <Head>
+    <title>{title ? `${title} - Ivan Atias` : defaultTitle}</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1' />
+    <meta charSet='UTF-8' />
+    <meta name='description' content={description || defaultDescription} />
+    <meta
+      name='keywords'
+      content='Front-end Engineer, UI Designer, Ivan Atias, Portfolio, Blog'
+    />
+    <meta name='author' content='Ivan Atias' />
+    <link
+      rel='canonical'
+      href={`https://www.ivanatias.codes${canonicalUrlPath || ''}`}
+    />
+    <meta
+      property='og:url'
+      content={`https://www.ivanatias.codes${canonicalUrlPath || ''}`}
+    />
+    <meta
+      property='og:title'
+      content={title ? `${title} - Ivan Atias` : defaultTitle}
+    />
+    <meta
+      property='og:description'
+      content={description || defaultDescription}
+    />
+    <meta name='twitter:card' content='summary_large_image' />
+    <meta property='og:site_name' content='Ivan Atias Website' />
+    <meta
+      name='twitter:title'
+      content={title ? `${title} - Ivan Atias` : defaultTitle}
+    />
+    <meta
+      name='twitter:description'
+      content={description || defaultDescription}
+    />
+    <meta
+      name='image'
+      property='og:image'
+      content={socialCardImage || defaultImage}
+    />
+    <meta name='twitter:image' content={socialCardImage || defaultImage} />
+    <meta property='og:type' content={contentType || defaultOgType} />
+  </Head>
+)
 
 export default MetaData
